05edcc934aa32d079121ebd7bd31a39653773bc3
Jayoda Halloluwa <jshalloluwa@students.nsbm.ac.lk>
2022-05-31 12:05:53 +0530

    Add Footer component.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Data from "../../utils/Data";
+import { Typography } from "@mui/material";
+
+import "./Footer.css";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <Typography className="footer_copyright">
+          Designed and Developed by <a href="/">{Data.profile_info.Name}</a>.
+          <br />
+          Clone idea from tavonline.
+        </Typography>
+      </div>
+      <div className="footer_right">
+        {Object.keys(Data.socials).map((media, index) => (
+          <a key={index} href={Data.socials[media].link} target="_blank" rel="noreferrer">{Data.socials[media].icon}</a>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
