e451b7751afe72ef064e2e9fe30bb01492bd47e5
Jayoda Halloluwa <jshalloluwa@students.nsbm.ac.lk>
2022-06-01 10:55:08 +0530

    Add Resume component.

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,122 @@
+import React from "react";
+import Data from "../../utils/Data";
+import CustomTimeline, { CustomTimelineSeparator } from "../Timeline/Timeline";
+import { Grid, Typography } from "@mui/material";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineContent from "@mui/lab/TimelineContent";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
+import Paper from "@mui/material/Paper";
+
+import "./Resume.css";
+import { TimelineDot } from "@mui/lab";
+
+const CustomItem = (props) => (
+  <TimelineItem>
+    <CustomTimelineSeparator />
+    <TimelineContent className="timeline_content">
+      <Typography className="timeline_title">{props.title}</Typography>
+      <Typography variant="caption" className="timeline_subtitle">
+        {props.caption}
+      </Typography>
+      <Typography variant="body2" className="timeline_description">
+        {props.description}
+      </Typography>
+    </TimelineContent>
+  </TimelineItem>
+);
+
+function Resume() {
+  return (
+    <div className="resume container_shadow">
+      {/* career objective */}
+      <Grid container className="section pb_45 pt_45">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6>Career Objective</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" className="objective_text">
+            {Data.objective}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      {/* education and projects */}
+      <Grid container className="section pb_45">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6>Qualifications</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container>
+            {/* education */}
+            <Grid item md={6}>
+              <CustomTimeline icon={<SchoolIcon />} header="Education">
+                {Data.education.map((item, index) => (
+                  <CustomItem
+                    key={index}
+                    title={item.title}
+                    caption={item.sub_title}
+                    description={item.description}
+                  />
+                ))}
+              </CustomTimeline>
+            </Grid>
+            {/* projects */}
+            <Grid item md={6}>
+              <CustomTimeline icon={<WorkIcon />} header="Projects">
+                {Data.projects.map((item, index) => (
+                  <CustomItem
+                    key={index}
+                    title={item.title}
+                    caption={item.sub_title}
+                    description={item.description}
+                  />
+                ))}
+              </CustomTimeline>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* skills */}
+      <Grid container className="section pb_45">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6>Skills</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container spacing={3} className="pb_45">
+            {Data.skills.map((skill, index) => (
+              <Grid item sm={6} md={4} key={index}>
+                <Paper variant="outlined" className="skill">
+                  <Typography variant="h6" className="skills_title">
+                    {skill.title}
+                  </Typography>
+                  {skill.elements.map((element, index) => (
+                    <Typography
+                      variant="body2"
+                      className="skill_content"
+                      key={index}
+                    >
+                      <TimelineDot
+                        variant="outlined"
+                        className="timeline_dot"
+                      />
+                      {element}
+                    </Typography>
+                  ))}
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Resume;
--- src/utils/Data.js
+++ src/utils/Data.js
@@ -22,6 +22,67 @@ const Data = {
       icon: <GitHubIcon />,
     },
   },
+
+  objective:
+    "I am Jayoda Halloluwa, who aims to be able to find an exciting and challenging entry level position in the Information and Technology industry alongside a company who will continuously motivate and drive me to do my best and to improve skills and abilities in order to be able to assist the company in achieving its company mission and goals.",
+
+  education: [
+    {
+      title: "Bachelors Degree",
+      sub_title: "2019 - Present",
+      description:
+        "BSc (Honours) in Software Engineering, NSBM Green University",
+    },
+    {
+      title: "GCE Advanced level",
+      sub_title: "2016 - 2019",
+      description: "Samudradevi Balika Vidyalaya, Nugegoda",
+    },
+    {
+      title: "GCE Ordinary level",
+      sub_title: "2005 - 2015",
+      description: "Samudradevi Balika Vidyalaya, Nugegoda",
+    },
+  ],
+
+  projects: [
+    {
+      title: "Project Noteff",
+      sub_title: "Personal data management mobile application",
+      description:
+        "Android, Kotlin, Material Design Library, SQLite, Android Room, MVVM",
+    },
+    {
+      title: "Project MCAT",
+      sub_title: "Vehicle hiring management system",
+      description: "C#, .NET core, MySQL, MVC, Dapper ORM",
+    },
+    {
+      title: "Student Management System",
+      sub_title: "CP/H/ Dhammaloka Vidyalaya",
+      description: "HTML, Bootstrap, PHP, MySQL, Codeigniter framework",
+    },
+  ],
+
+  skills: [
+    {
+      title: "Development Skills",
+      elements: ["HTML", "CSS", "JavaScript", "React JS", "Kotlin"],
+    },
+    {
+      title: "Soft Skills",
+      elements: [
+        "Communicational",
+        "Time Management",
+        "Teamwork",
+        "Analytical",
+      ],
+    },
+    {
+      title: "Languages",
+      elements: ["Sinhala", "English"],
+    },
+  ],
 };
 
 export default Data;
